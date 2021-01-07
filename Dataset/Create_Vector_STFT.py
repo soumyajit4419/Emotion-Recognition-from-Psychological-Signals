0479844991f2ea767aff7246cf9abf385a547f99
Rahul1582 <rahulkpatro@gmail.com>
2021-01-07 21:08:27 +0530

    implemented CNN code

---
+++ Dataset/Create_Vector_STFT.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import pickle
+import os
+
+nums=[2,3,4,5,6,7]
+
+path = './STFTData/'
+
+all_data= os.listdir(path)
+
+final_data_sfft=[]
+final_labels_sfft=[]
+
+for data in all_data:
+    with open( path + data , 'rb') as file:
+        sub = np.load(file , allow_pickle=True)
+        
+        for i in range (0,sub.shape[0]):
+            final_data_sfft.append(sub[i][0])
+            final_labels_sfft.append(sub[i][1])
+
+
+np.array(final_data_sfft).shape
+
+np.array(final_labels_sfft).shape
+
+np.save(path + 'SFFTFinalData', np.array(final_data_sfft), allow_pickle=True, fix_imports=True)
+
+np.save(path + 'SFFTFinalLabels', np.array(final_labels_sfft), allow_pickle=True, fix_imports=True)
